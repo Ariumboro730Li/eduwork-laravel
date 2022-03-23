7eef6c8d01531d71297d95e8044a4151b37bf4b5
aiwantDev <irawanyodi06@gmail.com>
2022-03-24 00:56:07 +0700

    Membuat tabel lainnya dengan berbagai relasi disertai dengan penjelasan

--- awan/apotek.sql
+++ awan/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2022 at 03:23 PM
+-- Generation Time: Mar 23, 2022 at 06:54 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `apotek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id` int(11) NOT NULL,
+  `id_penjualan` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -46,9 +60,54 @@ CREATE TABLE `obat` (
   `harga_jual` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `no_hp` varchar(13) NOT NULL,
+  `alamat` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan_jual`
+--
+
+CREATE TABLE `pelanggan_jual` (
+  `id` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `id_penjualan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id` int(11) NOT NULL,
+  `tgl_penjualan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_penjualan` (`id_penjualan`),
+  ADD KEY `id_obat` (`id_obat`);
 
 --
 -- Indexes for table `jenis_obat`
@@ -64,8 +123,34 @@ ALTER TABLE `obat`
   ADD KEY `fk_kategori` (`kd_kategori`);
 
 --
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pelanggan_jual`
+--
+ALTER TABLE `pelanggan_jual`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_penjualan` (`id_penjualan`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jenis_obat`
@@ -80,14 +165,46 @@ ALTER TABLE `obat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pelanggan_jual`
+--
+ALTER TABLE `pelanggan_jual`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD CONSTRAINT `detail_penjualan_ibfk_1` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id`),
+  ADD CONSTRAINT `detail_penjualan_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
 
 --
 -- Constraints for table `obat`
 --
 ALTER TABLE `obat`
   ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`kd_kategori`) REFERENCES `jenis_obat` (`id`);
+
+--
+-- Constraints for table `pelanggan_jual`
+--
+ALTER TABLE `pelanggan_jual`
+  ADD CONSTRAINT `pelanggan_jual_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`),
+  ADD CONSTRAINT `pelanggan_jual_ibfk_2` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
