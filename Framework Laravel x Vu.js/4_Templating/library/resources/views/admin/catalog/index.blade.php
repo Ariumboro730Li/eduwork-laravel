995bf7dcaa35f7905130e70d615abdcfa0a0cfb2
adis354 <ayatullahadisasono08@gmail.com>
2022-05-21 13:45:10 +0700

    compact data

--- Framework Laravel x Vu.js/4_Templating/library/resources/views/admin/catalog/index.blade.php
+++ Framework Laravel x Vu.js/4_Templating/library/resources/views/admin/catalog/index.blade.php
@@ -3,5 +3,50 @@
 
 
 @section('content')
-Ini adalah halaman catalog
+<section class="content">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-6">
+<div class="card">
+<div class="card-header">
+<h3 class="card-title">Data Catalog</h3>
+</div>
+ 
+<div class="card-body">
+<table class="table table-bordered">
+<thead>
+<tr>
+<th style="width: 10px">#</th>
+<th class="text-center">Name</th>
+<th class="text-center">Created At</th>
+<th class="text-center">Updated At</th>
+<th class="text-center">Total Books</th>
+
+</tr>
+</thead>
+<tbody>
+    @foreach($catalogs as $key => $catalog)
+<tr>
+<td>{{ $key+1 }}</td>
+<td class="text-center">{{ $catalog->name }}</td>
+<td class="text-center">{{ date('H:i:s - d M Y', strtotime ($catalog->created_at)) }}</td>
+<td class="text-center">{{ date('H:i:s - d M Y', strtotime ($catalog->updated_at)) }}</td>
+<td class="text-center">{{ count ($catalog->books) }}</td>
+</tr>
+    @endforeach
+
+</tbody>
+</table>
+</div>
+
+<div class="card-footer clearfix">
+<ul class="pagination pagination-sm m-0 float-right">
+<li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+<li class="page-item"><a class="page-link" href="#">1</a></li>
+<li class="page-item"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>
+<li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+</ul>
+</div>
+</div>
 @endsection
