048b0b0e37a793945ec21c2ef733eeac72970c90
adis354 <ayatullahadisasono08@gmail.com>
2022-05-24 15:41:09 +0700

    forgot password

--- Framework Laravel x Vu.js/4_Templating/library/resources/views/auth/login.blade.php
+++ Framework Laravel x Vu.js/4_Templating/library/resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
       </form>
 
       <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
+        <a href="{{ route('password.request') }}">I forgot my password</a>
       </p>
       <p class="mb-0">
         <a href="register" class="text-center">Register a new membership</a>
