0145c1a20d7b0045f1e8e595dcadae11c5eae998
adis354 <ayatullahadisasono08@gmail.com>
2022-05-31 21:05:16 +0700

    perbaikan fungsi

--- Review PHP dan MySQL/11_Add Bootstrap/add_katalog.php
+++ Review PHP dan MySQL/11_Add Bootstrap/add_katalog.php
@@ -1,32 +1,50 @@
-<html>
-<head>
-	<title>Add Katalog</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</head>
+	<?php
+	session_start();
+
+	include_once "function.php";
+
+	include_once("connect.php");
+
+	// Check If form submitted, insert form data into users table.
+	if (isset($_POST['Submit'])) {
+		$id_katalog = $_POST['id_katalog'];
+		$nama = $_POST['nama'];
+
+		$result = mysqli_query($mysqli, "INSERT INTO `katalog` (`id_katalog`, `nama`) VALUES ('$id_katalog', '$nama');");
+
+		// if data inserted successfully return to index page if set error
+		if ($result) {
+			// set success message to index page
+			set_flashdata('success', 'katalog berhasil ditambahkan.');
+			header("Location:index_katalog.php");
+		} else {
+			$errors['db'] = "insert failed something wrong :(";
+		}
+	}
+	?>
+	<html>
+
+	<head>
+		<title>Add Katalog</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	</head>
 
 <center class='bg-dark'>
     <a class='btn btn-dark' href="index.php">Buku</a> |
     <a class='btn btn-dark' href="index_penerbit.php">Penerbit</a> |
     <a class='btn btn-dark' href="index_pengarang.php">Pengarang</a> |
     <a class='btn btn-dark' href="index_katalog.php">Katalog</a>
+    <a class='btn btn-dark' href="index_pinjam.php">Pinjam</a>  
     <hr>
 </center>
 
-<?php
-	include_once("connect.php");
-    $penerbit = mysqli_query($mysqli, "SELECT * FROM penerbit");
-    $pengarang = mysqli_query($mysqli, "SELECT * FROM pengarang");
-    $katalog = mysqli_query($mysqli, "SELECT * FROM katalog");
-?>
- 
-<body>
-<tr class="container">
-	<a class="btn btn-primary" href="index_katalog.php">Go Home</a>
-	<br/><br/>
- 
-	<form action="add_katalog.php" method="post" name="form1">
-		<table width="90%" border="0">
+	<body>
+		<div class="container">
+		<a class="btn btn-primary" href="index_katalog.php">Go Home</a>
+		<br/><br/>
+
+
+			<form style="width: 50%;" action="" method="post">
 			<tr> 
 				<td><label for="id_katalog">ID Katalog</label></td>
 				<td><input type="text" class="form-control" id="id_katalog" name="id_katalog" required></td>
@@ -34,27 +52,15 @@
 			<tr> 
 			<td><label for="nama">Nama Katalog</label></td>
 				<td><input type="text" class="form-control" id="nama" name="nama" required></td>
-			</tr>
+			</tr></br>
 			<tr> 
 				<td></td>
-				<td><input type="submit" name="Submit" class="btn btn-primary" value="Add"></td>
+				<td><input type="submit" name="Submit" class="btn btn-success" value="Add"></td>
 			</tr>
-		</table>
-	</form>
-	
-	<?php
-	 
-		// Check If form submitted, insert form data into users table.
-		if(isset($_POST['Submit'])) {
-			$id_katalog = $_POST['id_katalog'];
-			$nama_katalog = $_POST['nama'];
-			
-			include_once("connect.php");
+			</form>
+		</div>
 
-			$result = mysqli_query($mysqli, "INSERT INTO `katalog` (`id_katalog`, `nama`) VALUES ('$id_katalog', '$nama');");
-			
-			header("Location:index_katalog.php");
-		}
-	?>
-</body>
-</html>
+
+	</body>
+
+	</html>
