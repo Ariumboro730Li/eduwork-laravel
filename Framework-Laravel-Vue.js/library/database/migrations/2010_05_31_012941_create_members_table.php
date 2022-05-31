043ebf8e74ab9ebbd93c704c71de31f6b6c35cc7
Achmad Fathoni <fathoniachmad43@gmail.com>
2022-05-31 12:12:51 +0700

    migration adn model

--- Framework-Laravel-Vue.js/library/database/migrations/2010_05_31_012941_create_members_table.php
+++ Framework-Laravel-Vue.js/library/database/migrations/2010_05_31_012941_create_members_table.php
@@ -13,17 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('member_id')->nullable();
+            $table->string('name', 64);
+            $table->char('gender', 1);
+            $table->char('phone_number', 15);
+            $table->text('address');
+            $table->string('email', 64);
             $table->timestamps();
-
-            $table->foreign('member_id')->references('id')->on('members');
+            
         });
     }
 
@@ -34,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 };
