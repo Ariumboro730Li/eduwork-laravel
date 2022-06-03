3ccf7ab3a6cec4271862019b7e35815fcc18d633
adis354 <ayatullahadisasono08@gmail.com>
2022-06-04 02:38:12 +0700

    forgot password

--- Laravel POS/tokoku/resources/views/auth/login.blade.php
+++ Laravel POS/tokoku/resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
 
       <!-- /.social-auth-links -->
       <p class="mb-1">
-        <a href="forgot-password">I forgot my password</a>
+        <a href="{{ route('password.request') }}">I forgot my password</a>
       </p>
       <p class="mb-0">
         <a href="register" class="text-center">Register a new membership</a>
