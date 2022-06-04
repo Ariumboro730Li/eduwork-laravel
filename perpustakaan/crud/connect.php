40b644ee51578cf26cee0e276e2d90f833178182
Achmad Fathoni <fathoniachmad43@gmail.com>
2022-06-04 11:30:16 +0700

    CRUD

--- perpustakaan/crud/connect.php
+++ perpustakaan/crud/connect.php
@@ -4,26 +4,6 @@ $database = "perpustakaan";
 $username = "root";
 $password = "";
 
-// create connection
-$cann = mysqli_connect($servername, $username, $password, $database);
-
-//check connection
-if (!$cann) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-//echo "Connected succesfully";
-//mysqli_close($cann);
-
-$sql = "SELECT * FROM `anggota`";
-$result = $cann ->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "anggota : " . $row["nama"] . " Beralamat di " . $row["alamat"] . "<br>";
-    }
-} else {
-    echo "Data kosong";
-
-}
-
+// Create connection
+$mysql = mysqli_connect($servername, $username, $password, $database);
 ?>
--- perpustakaan/crud/pengarang/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "perpustakaan";
-$username = "root";
-$password = "";
-
-// Create connection
-$mysql = mysqli_connect($servername, $username, $password, $database);
-?>
