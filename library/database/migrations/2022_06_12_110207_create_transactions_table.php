a784831c6609850185b54393c02f3c6847224457
superflatguy <prasetyadms7@gmail.com>
2022-06-12 20:01:37 +0800

    Migration and Model

--- library/database/migrations/2022_06_12_110207_create_transactions_table.php
+++ library/database/migrations/2022_06_12_110207_create_transactions_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('member_id');
+            $table->date('date_start');
+            $table->date('date_end');
             $table->timestamps();
+
+            $table->foreign('member_id')->references('id')->on('members');
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
