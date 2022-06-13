c9e786af7b45835cf648fb4b926ecc05af8553f5
superflatguy <prasetyadms7@gmail.com>
2022-06-13 09:56:59 +0800

    DB Seed

---
+++ library/database/seeders/MemberSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Member;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i=0; $i<20; $i++){
+            $member = new Member;
+
+            $member->name = $faker->name;
+            $member->gender = $faker->randomElement(['M', 'F']);
+            $member->phone_number = '0821'.$faker->randomNumber(8);
+            $member->address = $faker->address;
+            $member->email = $faker->email;
+            $member->created_at = $faker->date;
+            $member->updated_at = $faker->date;
+
+            $member->save();
+        }
+    }
+}
