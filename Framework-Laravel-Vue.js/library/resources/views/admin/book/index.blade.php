d6fb61f887f76b87fef8d3ee769a00109bbb6bbb
Achmad Fathoni <fathoniachmad43@gmail.com>
2022-06-25 08:33:53 +0700

    CRUD (with vue js part IV)

--- Framework-Laravel-Vue.js/library/resources/views/admin/book/index.blade.php
+++ Framework-Laravel-Vue.js/library/resources/views/admin/book/index.blade.php
@@ -22,7 +22,7 @@
 
     <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12" v-for="book in filteredList">
-            <div class="info-box">
+            <div class="info-box" v-on:click="editData(book)">
                 <div class="info-box-content">
                     <span class="info-box-text h3">@{{ book.title }} ( @{{ book.qty}} )</span>
                     <span class="info-box-number">Rp. @{{ numberWithSpaces(book.price) }},-<small></small></span>
@@ -31,10 +31,10 @@
         </div>
     </div>
   
-    <!-- <div class="modal fade" id="modal-default">
+    <div class="modal fade" id="modal-default">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form method="post" :action="actiobUrl" autocomplete="off">
+                <form method="post" action="" autocomplete="off">
                     <div class="modal-header">
                         <h4 class="modal-title">Book</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -44,54 +44,69 @@
                     <div class="modal-body">
                         @csrf
 
+                    <input type="hidden" name="_method" value="PUT" v-if="editStatus">
+
                         <div class="form-group">
                             <label>ISBN</label>
-                            <input type="number" class="form-control" name="isbn" required="">
+                            <input type="number" class="form-control" name="isbn" required="" :value="book.isbn">
                         </div>     
 
                         <div class="form-group">
                             <label>Title</label>
-                            <input type="number" class="form-control" name="title" required="">
+                            <input type="text" class="form-control" name="title" required="" :value="book.title">
                         </div>     
 
                         <div class="form-group">
-                            <label>Tahun</label>
-                            <input type="number" class="form-control" name="year" required="">
+                            <label>Year</label>
+                            <input type="number" class="form-control" name="year" required="" :value="book.year">
                         </div>     
 
                         <div class="form-group">
                             <label>Publisher</label>
-                    
+                            <select name="publisher_id" class="form-control">
+                                @foreach($publishers as $publisher)
+                                <option :selected="book.publisher_id == {{ $publisher->id }}" value="{{ $publisher->id }}">{{ $publisher->name }}</option>
+                                @endforeach
+                            </select>
                         </div>     
 
                         <div class="form-group">
-                            <label>Pengarang</label>
-                            
+                            <label>Author</label>
+                            <select name="author_id" class="form-control">
+                                @foreach($authors as $author)
+                                <option :selected="book.author_id == {{ $author->id }}" value="{{ $author->id }}">{{ $author->name }}</option>
+                                @endforeach
+                            </select>
                         </div>     
 
                         <div class="form-group">
-                            <label>Katalog</label>
-                            
+                            <label>Catalog</label>
+                            <select name="catalog_id" class="form-control">
+                                @foreach($catalogs as $catalog)
+                                <option :selected="book.catalog_id == {{ $catalog->id }}" value="{{ $catalog->id }}">{{ $catalog->name }}</option>
+                                @endforeach
+                            </select>
                         </div>     
 
                         <div class="form-group">
                             <label>Qty Stock</label>
-                            <input type="number" class="form-control" name="qty" required="">
+                            <input type="number" class="form-control" name="qty" required="" :value="book.qty">
                         </div>     
 
                         <div class="form-group">
                             <label>Harga Pinjam</label>
-                            <input type="number" class="form-control" name="price" required="">
+                            <input type="number" class="form-control" name="price" required="" :value="book.price">
                         </div>     
                         <div class="modal-footer justify-content-between">
-                            <button type="button" class="btn btn-default bg-danger">Delete</button>
+                            <button type="button" class="btn btn-default bg-danger" v-if="editStatus" v-on:click="deleteData(book.id)">Delete</button>
                             <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>     
                     </div>     
                 </form>
             </div>
         </div>
-    </div> -->
+    </div>
+
 </div>
 @endsection
 
@@ -104,7 +119,9 @@
         el: '#controller',
         data: {
             books: [],
-            search:''
+            search:'',
+            book:{},
+            editStatus:false
         },
         mounted: function () {
             this.get_books();
@@ -123,9 +140,19 @@
                     }
                 });
             },
-            //  addData() {
-            //      $('#modal-default').modal();
-            //}
+            addData() {
+                this.book = {};
+                this.editStatus=false;
+                $('#modal-default').modal();
+            },
+            editData(book) {
+                this.book = book;
+                this.editStatus=true;
+                $('#modal-default').modal();
+            },
+            deleteData(id) {
+                console.log(id);
+            },
             numberWithSpaces(x) {
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
             }
