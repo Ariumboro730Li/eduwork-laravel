a4a9786b7993304ff0a48e34551d739b1088e003
aiwantDev <irawanyodi06@gmail.com>
2022-09-20 20:47:52 +0700

    Task Membuat grafik

--- awan/perpus-smk/app/Http/Controllers/HomeController.php
+++ awan/perpus-smk/app/Http/Controllers/HomeController.php
@@ -188,6 +188,37 @@ class HomeController extends Controller
 
         // return $data13;
 
-        return view('home');
+        $total_anggota = Member::count();
+        $total_buku = Book::count();
+        $total_peminjaman = Transaction::count();
+        $total_penerbit = Publisher::count();
+
+        $data_donat = Book::select(DB::raw("COUNT(publisher_id) as total"))->groupBy('publisher_id')->orderBy('publisher_id', 'asc')->pluck('total');
+        $label_donat = Publisher::orderBy('publishers.id', 'asc')->join('books', 'books.publisher_id', '=', 'publishers.id')->groupBy('name')->pluck('name');
+
+        $label_bar = ['Peminjaman', 'Pengembalian'];
+        $data_bar = [];
+
+        foreach ($label_bar as $key => $value) {
+            $data_bar[$key]['label'] = $label_bar[$key];
+            $data_bar[$key]['backgroundColor'] = $key == 0 ? '#2200ee' : '#9900aa';
+            $data_month = [];
+
+            foreach (range(1, 12) as $month) {
+                if ($key == 0) {
+                    $data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))->whereMonth('date_start', $month)->first()->total;
+                } else {
+                    $data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))->whereMonth('date_end', $month)->first()->total;
+                }
+            }
+
+            $data_bar[$key]['data'] = $data_month;
+        }
+
+        $label_area = Author::orderBy('authors.id', 'asc')->join('books', 'books.author_id', '=', 'authors.id')->groupBy('name')->pluck('name');
+        $data_area = Book::select(DB::raw("COUNT(author_id) as total"))->groupBy('author_id')->orderBy('author_id', 'asc')->pluck('total');
+
+        // return $data_month;
+        return view('admin.home', compact('total_anggota', 'total_buku', 'total_peminjaman', 'total_penerbit', 'data_donat', 'label_donat', 'data_bar', 'label_area', 'data_area'));
     }
 }
