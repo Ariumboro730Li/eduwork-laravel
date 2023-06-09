81a48e700257bece9e76209c6651531041462e8b
khozinkz <khozinkz48@gmail.com>
2023-06-10 04:19:07 +0700

    tugas 7 add database

--- tugas-php/tugas 7/index.php
+++ tugas-php/tugas 7/index.php
@@ -1,10 +1,10 @@
 <?php
     include_once("connect.php");
     $buku = mysqli_query($mysqli, "SELECT buku.*, nama_pengarang, nama_penerbit, katalog.nama as nama_katalog FROM buku 
-            LEFT JOIN  pengarang ON pengarang.id_pengarang = buku.id_pengarang
-            LEFT JOIN  penerbit ON penerbit.id_penerbit = buku.id_penerbit
-            LEFT JOIN  katalog ON katalog.id_katalog = buku.id_katalog
-            ORDER BY judul ASC");
+        LEFT JOIN  pengarang ON pengarang.id_pengarang = buku.id_pengarang
+        LEFT JOIN  penerbit ON penerbit.id_penerbit = buku.id_penerbit
+        LEFT JOIN  katalog ON katalog.id_katalog = buku.id_katalog
+        ORDER BY judul ASC");
 ?>
  
 <html>
@@ -17,10 +17,10 @@
 <body>
 
 <center>
-    <a href="index.php" class="btn btn-success">Buku</a> |
-    <a href="penerbit/index.php">Penerbit</a> |
-    <a href="pengarang/index.php">Pengarang</a> |
-    <a href="katalog/">Katalog</a>
+     <a href="index.php" class="btn btn-success">Buku</a> |
+    <a href="penerbit/index.php" >Penerbit</a> |
+    <a href="pengarang/index.php" >Pengarang</a> |
+    <a href="katalog/index.php" >Katalog</a>
     <hr>
 </center>
 
@@ -50,8 +50,7 @@
             echo "<td>".$buku_data['nama_katalog']."</td>";    
             echo "<td>".$buku_data['qty_stok']."</td>";    
             echo "<td>".$buku_data['harga_pinjam']."</td>";    
-            echo "<td><a class='btn btn-primary' href='buku/edit.php?isbn=$buku_data[isbn]'>Edit</a> 
-            |     <a class='btn btn-danger' href='buku/delete.php?isbn=$buku_data[isbn]'>Delete</a></td></tr>";        
+            echo "<td><a class='btn btn-primary' href='buku/edit.php?isbn=$buku_data[isbn]'>Edit</a> | <a class='btn btn-danger' href='buku/delete.php?isbn=$buku_data[isbn]'>Delete</a></td></tr>";        
         }
     ?>
     </table>
